5fbf3a3c58eab6417bf273131c240f938f3651ea
lemosadam <117216643+lemosadam@users.noreply.github.com>
2024-02-01 02:22:48 -0500

    sketch

--- assignments/01_abstract/sketch.js
+++ assignments/01_abstract/sketch.js
@@ -1,7 +1,68 @@
+let dots = [];
+let numDots = 4800;
+
+// p5 calls this function right away when the webpage is loaded
 function setup() {
- createCanvas(600, 400);
+  createCanvas(800, 600);
+  colorMode(HSB);
+  noStroke();
+  let x = -5;
+  let y = 5;
+  // Create an array of Dot objects (see the class below)
+  for (let i = 0; i < numDots; i++) {
+    // Make use of the iterator variable (i) to compute the x position of the dot
+    x = x + 10;
+    
+    if (x >= width)
+    {
+
+      x = 5
+      y = y + 10;
+    }
+    dots[i] = new Dot(x, y, 10, i);
+    
+  }
 }
 
+// The draw function is called over and over again really fast by p5
 function draw() {
-  background(0,0,100);
+  background(0, 0, 100, 0.08);
+
+  // Loop through the array of dots that we created on inside the start function, and 
+  // call their update function.
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].update();
+  }
+}
+
+
+
+// Below, we define the Dot class, which defined the objects we instantiate inside the
+// setup function above.
+class Dot {
+  constructor(x, y, diameter, id) {
+
+    this.position = createVector(x, y);
+
+    this.velocity = createVector(0, 0);
+
+    this.id = id;
+
+    this.diameter = 5;
+
+    this.hue = (0)
+
+    if (this.id > 2400)
+    {
+      this.hue = 180;
+    }
+  }
+
+  // This is the function that is called in the draw function above.
+  update() {
+  
+
+    fill(this.hue, 50, 100);
+    ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
+  }
 }
